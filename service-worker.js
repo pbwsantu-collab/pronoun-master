de7c6f5c498bcdef69b6fcd1193874e0329e146b
pbwsantu-collab <pbwsantu@gmail.com>
2026-09-13 13:00:03 +0530

    Complete Pronoun Master PWA - Lessons, Questions, CSS, JS, and PWA config

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'pronoun-master-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './data/lessons.json',
+  './data/questions.json',
+  './data/glossary.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .catch((err) => console.log('Cache failed:', err))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+      .catch(() => caches.match('./index.html'))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
